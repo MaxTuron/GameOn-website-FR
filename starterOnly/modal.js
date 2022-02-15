1aa45b5a940bc37e1dd29b4894d424671a85c41f
MaxTuron <@=@4d~Yx|bAiu|`Yv!$N>
2022-02-15 13:52:03 +0100

    Mise en place de la fermeture du modal avec la croix.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,21 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtnClose.forEach((span) => span.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//Close modal
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
@@ -28,28 +35,25 @@ function validate() {
       email = document.getElementById("email").value,
       birthdate = document.getElementById("birthdate").value,
       quantity = document.getElementById("quantity").value,
+      cityCollection = document.getElementsByName("location"),
       firstRegex = /[0-9]/.test(firstName),
       lastRegex = /[0-9]/.test(lastName),
-      emailRegex = /@/.test(email);
+      emailRegex = /@/.test(email),
+      quantityRegex = /\d/.test(quantity);
 
   let firstNameValid = false,
-   lastNameValid = false,
-   emailValid = false,
-   birthdateValid = false,
-   quantityValid = false;
-
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email);
-  console.log(birthdate);
-  console.log(quantity);
+      lastNameValid = false,
+      emailValid = false,
+      birthdateValid = false,
+      quantityValid = false,
+      checkboxValid = false;
 
 //Gestion du prénom
   if(firstName.length<2){
     alert("Le prénom doit faire plus de deux caractères.");
   }else if (firstName==="") {
     alert("Le prénom ne doit pas être vide.");
-  }else if(firstRegex===false){
+  }else if(firstRegex===true){
     alert("Le prenom ne doit pas contenir de nombres.");
   }else{
     firstNameValid = true;
@@ -60,7 +64,7 @@ function validate() {
     alert("Le nom doit faire plus de deux caractères.");
   }else if (lastName==="") {
     alert("Le nom ne doit pas être vide.");
-  }else if(lastRegex===false){
+  }else if(lastRegex===true){
     alert("Le nom ne doit pas contenir de nombres.");
   }else{
     lastNameValid = true;
@@ -77,15 +81,33 @@ function validate() {
 
   //Gestion de la date de naissance
   if (birthdate==="") {
-    alert("L'email ne doit pas être vide.");
+    alert("La date de naissance ne doit pas être vide.");
   }else {
     birthdateValid = true;
   }
 
+  //Gestion de la quantité de concours
+  if (quantity==="") {
+    alert("Le nombre de concours ne doit pas être vide.");
+  }else if(quantityRegex===false){
+    alert("Un nombre doit être saisi.");
+  }else {
+    quantityValid = true;
+  }
+
+//Gestion des villes
+
+//Vérification condition d'utilisation checked
+    if (document.getElementById('checkbox1').checked) {
+      checkboxValid = true;
+    }else{
+      alert ('Veuillez cocher la case des conditions d\'utilisations pour vous inscrire');
+    }
 
   //Verification finale de tous les champs
-  if(firstNameValid===true && lastNameValid===true && emailValid===true && birthdateValid===true && quantityValid===true){
-    alert("Form OK")
+  if(firstNameValid===true && lastNameValid===true && emailValid===true && birthdateValid===true && quantityValid===true && checkboxValid===true){
+    alert("Form OK"),
+    closeModal();
   }else{
     alert("Erreur")
   }
